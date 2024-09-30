38a91fd8361022f81053280adf6509b2d112aa0a
Greg <gregm31@live.com>
2024-10-01 04:16:16 +1000

    Hero slice complete

---
+++ src/lib/components/Bounded.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import clsx from 'clsx'
+	import type { Snippet } from 'svelte'
+
+	type BoundedProps = {
+		tag?: string
+		children: Snippet
+		class?: string
+		[key: string]: any
+	}
+
+	let {
+		tag = 'section',
+		children,
+		class: className,
+		...restProps
+	}: BoundedProps = $props()
+</script>
+
+<svelte:element
+	this={tag}
+	{...restProps}
+	class={clsx('px-4 py-14 first:pt-10 md:px-6 md:py-20 lg:py-24', className)}
+>
+	<div class="mx-auto flex w-full max-w-6xl flex-col items-center">
+		{@render children()}
+	</div>
+</svelte:element>
--- src/lib/slices/Hero/index.svelte
+++ src/lib/slices/Hero/index.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
-	import type { Content } from '@prismicio/client';
+	import { Bounded, TriangleGrid } from '$lib/components'
+	import type { Content } from '@prismicio/client'
+	import { PrismicImage, PrismicText, PrismicLink } from '@prismicio/svelte'
 
-	export let slice: Content.HeroSlice;
+	export let slice: Content.HeroSlice
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
-</section>
+<Bounded
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+>
+	<div class="relative text-center">
+		<TriangleGrid />
+		{#if slice.primary.heading}
+			<h1
+				class="text-balance max-w-3xl text-5xl font-medium md:text-7xl mx-auto"
+			>
+				<PrismicText field={slice.primary.heading} />
+			</h1>
+		{/if}
+		{#if slice.primary.body}
+			<p class="mx-auto mt-6 max-w-md text-balance text-gray-300">
+				<PrismicText field={slice.primary.body} />
+			</p>
+		{/if}
+		{#if slice.primary.button_link}
+			<PrismicLink class="mt-8" field={slice.primary.button_link}
+				>{slice.primary.button_label}</PrismicLink
+			>
+		{/if}
+		{#if slice.primary.image}
+			<div class="mt-16 w-fit glass-container">
+				<div
+					class="absolute left-1/3 mt-16 top-0 -z-10 h-2/3 w-2/3 bg-violet-700/50 mix-blend-screen blur-[120px] filter"
+				></div>
+				<div
+					class="absolute left-0 mt-16 top-1/3 -z-10 h-2/3 w-2/3 bg-orange-600/50 mix-blend-screen blur-[120px] filter"
+				></div>
+				<PrismicImage class="rounded-lg" field={slice.primary.image} />
+			</div>
+		{/if}
+	</div>
+</Bounded>
